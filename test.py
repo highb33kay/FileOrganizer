7a4a728126189a9c053e72ed422642c0a0261ff5
highb33kay <hiibeekayvibe@gmail.com>
2022-05-29 03:10:18 +0100

    reading the files and creating folder

--- test.py
+++ test.py
@@ -4,4 +4,8 @@ entries = Path("C:\\Users\\HighB33Kay\\Desktop\\sort")
 
 for entry in entries.iterdir():
     if entry.is_file():
+        name = entry.stem
+        ext = entry.suffix[1:]
+        Path(Path("out")/ext).mkdir(parents=True, exist_ok=True)
+        
         
